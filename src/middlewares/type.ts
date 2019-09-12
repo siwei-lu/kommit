6947422ae32393dfc23b49dd6959e5ca69a38d5c
Idan Loo <im@siwei.lu>
2019-09-12 17:48:06 +0800

    feat(type): add short names to make more readable

--- src/middlewares/type.ts
+++ src/middlewares/type.ts
@@ -10,39 +10,48 @@ const typeQuestion: ListQuestion = {
     {
       name: 'feat:\t\tA new feature',
       value: 'feat',
+      short: 'feat',
     },
     {
       name: 'fix:\t\tA bug fix',
       value: 'fix',
+      short: 'fix',
     },
     {
       name: 'test:\t\tAdding missing tests',
       value: 'test',
+      short: 'test',
     },
     {
       name: 'chore:\tBuild process or auxiliary tool changes',
       value: 'chore',
+      short: 'chore',
     },
     {
       name: 'docs:\t\tDocumentation only changes',
       value: 'docs',
+      short: 'docs',
     },
     {
       name:
         'refactor:\tA code change that neither fixes a bug or adds a feature',
       value: 'refactor',
+      short: 'refactor',
     },
     {
       name: 'style:\tMarkup, white-space, formatting, missing semi-colons...',
       value: 'style',
+      short: 'style',
     },
     {
       name: 'ci:\t\tCI related changes',
       value: 'ci',
+      short: 'ci',
     },
     {
       name: 'perf:\t\tA code change that improves performance',
       value: 'pref',
+      short: 'pref',
     },
   ],
 }
