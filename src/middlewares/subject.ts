7419ba59d383b4f0ea0784548bb1cbf91beac26e
Idan Loo <im@siwei.lu>
2019-09-14 20:05:14 +0800

    feat(ui): alert warning when the subject is over than 65 characters
    Co-authored-by: Kommit Tool <kommit@siwei.lu>

--- src/middlewares/subject.ts
+++ src/middlewares/subject.ts
@@ -2,20 +2,23 @@ import { Question, prompt } from 'inquirer'
 import { Context } from '~/types'
 import { Next } from '@idan-loo/middleware'
 
-export async function acquireSubject(ctx: Context, next: Next) {
-  const subjectQuestion: Question = {
-    type: 'input',
-    name: 'subject',
-    message: 'Write a short, imperative mood description of the change:',
-    suffix: ` (no more than 65 characters)\n ${ctx.type}:`,
+const subjectQuestion: Question = {
+  type: 'input',
+  name: 'subject',
+  message: 'Write a short, imperative mood description of the change:',
 
-    filter(input: string) {
-      if (input.length > 65) {
-        return input.substr(0, 65)
-      }
-      return input
-    },
-  }
+  validate(input: string) {
+    if (input.length > 65) {
+      return `should be fewer than: ${input.substr(0, 65)}`
+    }
+    return true
+  },
+}
+
+export async function acquireSubject(ctx: Context, next: Next) {
+  subjectQuestion.suffix = ` (no more than 65 characters)\n ${ctx.type}${
+    ctx.scope ? `(${ctx.scope})` : ''
+  }:`
 
   const { subject } = await prompt([subjectQuestion])
   ctx.subject = subject
