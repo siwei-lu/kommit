4d09ab36d61cb20653743cfabf476cbded921566
Idan Loo <im@siwei.lu>
2019-09-17 16:45:38 +0800

    feat(ui): add the None option to adjust when there's no co-author

--- src/middlewares/author.ts
+++ src/middlewares/author.ts
@@ -4,6 +4,8 @@ import { Context } from '~/types'
 import { getConfigIn } from '~/config'
 import { concatWithNewLines } from '~/helper'
 
+const None = 'None'
+
 const coAuthorQuestion: ListQuestion = {
   type: 'list',
   name: 'coAuthor',
@@ -14,9 +16,12 @@ export async function acquireCoAuthor(ctx: Context, next: Next) {
   const { members } = await getConfigIn(ctx.path)
 
   if (members) {
-    coAuthorQuestion.choices = members.map(e => ({ value: e }))
+    coAuthorQuestion.choices = [None, ...members.map(e => ({ value: e }))]
     const { coAuthor } = await prompt([coAuthorQuestion])
-    ctx.footer = concatWithNewLines(ctx.footer, `Co-authored-by: ${coAuthor}`)
+
+    if (coAuthor !== None) {
+      ctx.footer = concatWithNewLines(ctx.footer, `Co-authored-by: ${coAuthor}`)
+    }
   }
 
   return next()
